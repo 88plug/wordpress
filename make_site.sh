c94091c162fda675fcb3005ff7eadf9252ede7e4
88plug <19512127+88plug@users.noreply.github.com>
2018-08-09 01:24:21 -0700

    Missing domain and image bug

--- make_site.sh
+++ make_site.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
-printf "%25s%s" "Checking docker" " ---> "
-            dockerCheck=$(docker ps)
-            if (( $? != 0 )); then
-                echo "FAILURE"
-                echo
-                echo "Cannot reach Docker" 
-                sudo apt-get install -y docker.io
-            else
-                echo "OK"
+echo "Checking Docker Installation"
+dockerCheck=$(docker ps)
+	if (( $? != 0 )); then
+		echo "FAILURE"
+		echo
+		echo "Cannot reach Docker" 
+		sudo apt-get install -y docker.io
+	else
+		echo "OK"
+	fi
+dockerCheck=$(docker-compose --version)
+	if (( $? != 0 )); then
+		echo "FAILURE"
+		echo
+		echo "Cannot reach docker-compose" 
+		sudo apt-get install -y docker-compose
+	else
+		echo "OK"
 fi
-printf "%25s%s" "Checking docker-compose" " ---> "
-            dockerCheck=$(docker-compose --version)
-            if (( $? != 0 )); then
-                echo "FAILURE"
-                echo
-                echo "Cannot reach docker-compose" 
-                sudo apt-get install -y docker-compose
-            else
-                echo "OK"
-fi
-echo "Please enter your email used for SSL"
+echo "Please enter your email for SSL certificates from CertBot"
 read email
 echo "You entered $email"
-echo "Please enter your primary domain now like : mydomain.com "
+echo "Please enter your primary domain name like : mydomain.com "
 read domain
 echo "You entered $domain"
-echo "Please enter your monitor domain now like : monitor.mydomain.com "
+echo "Please enter your monitor domain name like : monitor.mydomain.com "
 read monitor_domain
 echo "You entered $monitor_domain"
 
@@ -35,12 +34,12 @@ DBNAME=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 6)
 mkdir $domain
 mkdir -p $domain/wp-content
 
-echo "Made new $domain directory"
-
+echo "Made new $domain directory with source files"
 sed -i 's/MONITOR/'$monitor_domain'/g' restart.sh
 sed -i 's/DOMAIN/'$domain'/g' traefik.toml
 sed -i 's/EMAIL/'$email'/g' traefik.toml
 sed -i 's/MYSQLROOTPASS/'$MYSQLROOTPASS'/g' docker-compose.yml
+sed -i 's/DOMAIN/'$DOMAIN'/g' docker-compose.yml
 sed -i 's/DBNAME/'$DBNAME'/g' docker-compose.yml
 
 echo "Moving things into place..."
@@ -53,5 +52,4 @@ cd $domain ; touch acme.json ; chmod 600 acme.json ; chmod +x ./restart.sh
 echo "Starting in 5 seconds..."
 sleep 5
 ./restart.sh
-
-echo "Your site $domain is now ready, please run $domain/restart.sh"
+echo "Your site $domain is now ready."
